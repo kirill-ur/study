2676a0319faa715c7a6398a50c102a87452889bb
kirill-ur <107951737+kirill-ur@users.noreply.github.com>
2022-07-08 22:40:02 +0300

    Update and rename README.md to Bot.js
    1. Хранить ключевые фразы в массиве
    2. Рандомно может доставить фразы
    3. Вводит в поле поиска и переходит на выдачу
    4. Находит целевой сайт и переходит на него

---
+++ Bot.js
@@ -0,0 +1,34 @@
+# study
+// ==UserScript==
+// @name         Google-bot
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  For google
+// @author       Kirill Vedernikov
+// @match        https://www.google.com/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+let keywords = ["10 самых популярных шрифтов от Google", "вывод произвольных полей wordpress", "Отключение редакций и ревизий"];
+let keyword = keywords[getRandom(0, keywords.lenght)];
+let links = document.links;
+let googleInput = document.getElementsByName("q")[0];
+let btnK = document.getElementsByName("btnK")[0];
+
+if (btnK !== undefined) {
+ googleInput.value = keyword;
+ btnK.click();
+}else {
+for (let i = 0; i < links.length; i++) {
+  if (links[i].href.includes("napli.ru")) {
+  let link = links[i];
+  console.log("Нашел строку" + links[i]) ;
+  link.click();
+  break;
+     }
+   }
+}
+function getRandom (min,max) {
+ return Math.floor(Math.random()*(max - min) + min)
+}
